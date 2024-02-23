4327e15c38f60324067f5454401fac0a94c67a95
austiee <austin1130@live.com>
2024-02-22 23:21:31 -0600

    final push

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,15 +1,37 @@
-
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+  }
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{ setFeatPupId(puppy.id)}}>
+            {puppy.name}
+          </p>
+        );
+      })}
 
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
 export default App
